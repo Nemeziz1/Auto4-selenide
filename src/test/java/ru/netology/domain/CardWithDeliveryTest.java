e20312d820d465e35b49d822389e48ff376ab928
Andrey Koryakin <football2331lm@gmail.com>
2020-11-29 22:01:46 +0300

    CardWithDelivery ready

--- src/test/java/ru/netology/domain/CardWithDeliveryTest.java
+++ src/test/java/ru/netology/domain/CardWithDeliveryTest.java
@@ -6,16 +6,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.SplittableRandom;
+
 
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardWithDeliveryTest {
     @BeforeEach
@@ -46,11 +42,11 @@ public class CardWithDeliveryTest {
         $(byText("Санкт-Петербург")).click();
         request.$("[class='icon-button__text'] [class='icon icon_size_m icon_name_calendar icon_theme_alfa-on-white']").click();
         request.$("[class='input__box'] [placeholder='Дата встречи']").click();
-        request.$("[class='calendar__row'] [data-day='1607202000000']").click();
+        $$("td.calendar__day").find(Condition.exactText("6")).click();
         request.$("[data-test-id=name] input.input__control").setValue("Корякин Андрей");
         request.$("[class='input__box'] [name='phone']").setValue("+79999999999");
         request.$("[data-test-id=agreement]").click();
         request.$("[class='button__content'] [class='button__text']").click();
-        $(byText("Успешно!")).waitUntil(Condition.visible, 15000);
+        $(byText("06.12.2020")).waitUntil(Condition.visible, 15000);
     }
 }
