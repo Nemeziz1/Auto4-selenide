97703284d0ecbf066ad90e8e677f2b968d7f38d4
Andrey Koryakin <football2331lm@gmail.com>
2020-12-01 21:13:02 +0300

    CardWithDelivery fixed

--- src/test/java/ru/netology/domain/CardWithDeliveryTest.java
+++ src/test/java/ru/netology/domain/CardWithDeliveryTest.java
@@ -10,7 +10,9 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardWithDeliveryTest {
@@ -32,7 +34,7 @@ public class CardWithDeliveryTest {
         request.$("[class='input__box'] [name='phone']").setValue("+79999999999");
         request.$("[data-test-id=agreement]").click();
         request.$("[class='button__content'] [class='button__text']").click();
-        $(byText(dateInForm)).waitUntil(Condition.visible, 15000);
+        $("[data-test-id=notification]").waitUntil(Condition.visible, 15000).shouldHave(exactText("Успешно! Встреча успешно забронирована на " + dateInForm));
     }
 
     @Test
@@ -42,11 +44,46 @@ public class CardWithDeliveryTest {
         $(byText("Санкт-Петербург")).click();
         request.$("[class='icon-button__text'] [class='icon icon_size_m icon_name_calendar icon_theme_alfa-on-white']").click();
         request.$("[class='input__box'] [placeholder='Дата встречи']").click();
-        $$("td.calendar__day").find(Condition.exactText("6")).click();
+        LocalDate date = LocalDate.now();
+        LocalDate dateMeet = date.plusDays(7);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String dateInForm = dateMeet.format(dateTimeFormatter);
+        int dayMeet = dateMeet.getDayOfMonth();
+        int deliveryMonth = dateMeet.getMonthValue();
+        int monthForDelivery = date.getMonthValue();
+        if (deliveryMonth != monthForDelivery) {
+            $("[class='popup__container'] [data-step='1']").click();
+        }
+        $(byText(String.valueOf(dayMeet))).click();
         request.$("[data-test-id=name] input.input__control").setValue("Корякин Андрей");
         request.$("[class='input__box'] [name='phone']").setValue("+79999999999");
         request.$("[data-test-id=agreement]").click();
         request.$("[class='button__content'] [class='button__text']").click();
-        $(byText("06.12.2020")).waitUntil(Condition.visible, 15000);
+        $("[data-test-id=notification]").waitUntil(Condition.visible, 15000).shouldHave(exactText("Успешно! Встреча успешно забронирована на " + dateInForm));
+    }
+
+    @Test
+    void shouldSubmitRequestOnTheNextMonth() {
+        SelenideElement request = $(".form");
+        request.$("[class='input__inner'] [type='text']").setValue("Сан");
+        $(byText("Санкт-Петербург")).click();
+        request.$("[class='icon-button__text'] [class='icon icon_size_m icon_name_calendar icon_theme_alfa-on-white']").click();
+        request.$("[class='input__box'] [placeholder='Дата встречи']").click();
+        LocalDate date = LocalDate.now();
+        LocalDate dateMeet = date.plusDays(45);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String dateInForm = dateMeet.format(dateTimeFormatter);
+        int dayMeet = dateMeet.getDayOfMonth();
+        int deliveryMonth = dateMeet.getMonthValue();
+        int monthForDelivery = date.getMonthValue();
+        if (deliveryMonth != monthForDelivery) {
+            $("[class='popup__container'] [data-step='1']").click();
+        }
+        $(byText(String.valueOf(dayMeet))).click();
+        request.$("[data-test-id=name] input.input__control").setValue("Корякин Андрей");
+        request.$("[class='input__box'] [name='phone']").setValue("+79999999999");
+        request.$("[data-test-id=agreement]").click();
+        request.$("[class='button__content'] [class='button__text']").click();
+        $("[data-test-id=notification]").waitUntil(Condition.visible, 15000).shouldHave(exactText("Успешно! Встреча успешно забронирована на " + dateInForm));
     }
 }
